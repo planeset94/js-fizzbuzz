e668dac175bd74310e6902e57016a8289ef43993
planeset94 <planeset94@gmail.com>
2021-04-27 16:46:43 +0200

    new For cicle without Array. New format

--- asset/JS/msin.js
+++ asset/JS/msin.js
@@ -6,20 +6,37 @@
 // Creo un'array in cui imagazzinare i valori 
 var list= new Array();
 
+// for (var i=1; i<=100; i++){
+//     if (i % 3 == 0 && i % 5 ==0){
+//         list.push("FizzBuzz");
+
+//     } else if (i % 5 ==0) {
+//     list.push("Buzz");
+
+//     } else if (i % 3 ==0){
+//         list.push("Fizz");
+
+
+//     }  else{
+//         list.push(i);
+//     }
+// }
+
+// document.getElementById('stampa').innerHTML=list.join('<br>');
+
+
 for (var i=1; i<=100; i++){
     if (i % 3 == 0 && i % 5 ==0){
-        list.push("FizzBuzz");
+        document.getElementById('stampa').innerHTML+= "<li class='red'>FizzBuzz</li>";
 
     } else if (i % 5 ==0) {
-    list.push("Buzz");
+        document.getElementById('stampa').innerHTML+="<li class='blue'>Buzz</li>";
 
     } else if (i % 3 ==0){
-        list.push("Fizz");
+        document.getElementById('stampa').innerHTML+="<li class='violet'>Fizz</li>";
 
 
     }  else{
-        list.push(i);
+        document.getElementById('stampa').innerHTML+="<li>"+i+"</li>";
     }
 }
-
-document.getElementById('stampa').innerHTML=list.join('<br>');
